3ff0f182f00af6164f9f6ba86ce8461e14eb31d5
paulchrisc <128294135+paulchrisc@users.noreply.github.com>
2023-11-09 01:32:42 +0800

    generated columns

--- 17-primary-key.sql
+++ 17-primary-key.sql
@@ -2,26 +2,30 @@
 -- DROP TABLE employers;
 -- DROP TABLE conversation;
 
--- CREATE type employment_status as ENUM('self-employed', 'employed', 'unemployed'); -- Postgresql
+-- Define the custom ENUM type 'employment_status' in PostgreSQL
+CREATE TYPE employment_status AS ENUM ('self-employed', 'employed', 'unemployed');
 
+-- Create the 'users' table using the custom ENUM type
 CREATE TABLE users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    full_name VARCHAR(300) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(200) NOT NULL,
+    last_name VARCHAR(200) NOT NULL,
+    full_name VARCHAR(401) GENERATED ALWAYS AS (CONCAT(first_name, ' ', last_name)),
     yearly_salary INT CHECK (yearly_salary > 0),
-    current_status ENUM('self-employed', 'employed', 'unemployed') -- MySQL
-    -- current_status employment_status
+    current_status employment_status
 );
 
+-- Create the 'employers' and 'conversation' tables
 CREATE TABLE employers (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     company_name VARCHAR(300) NOT NULL,
     company_address VARCHAR(300) NOT NULL,
-    yearly_revenue FLOAT CHECK (yearly_revenue <0),
+    yearly_revenue FLOAT CHECK (yearly_revenue > 0),
     is_hiring BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE conversation (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     user_id INT,
     employer_id INT,
     message TEXT NOT NULL
---
+++ 18-generate-columns.sql
@@ -0,0 +1,36 @@
+-- DROP TABLE users;
+-- DROP TABLE employers;
+-- DROP TABLE conversation;
+
+-- CREATE type employment_status as ENUM('self-employed', 'employed', 'unemployed'); -- Postgresql
+
+-- CREATE TABLE users (
+--     -- id SERIAL PRIMARY KEY, -- posgresql
+--     id INT PRIMARY KEY AUTO_INCREMENT, -- mySQL
+--     first_name VARCHAR(200) NOT NULL,
+--     last_name VARCHAR(200) NOT NULL,
+--     full_name VARCHAR(401) GENERATED ALWAYS AS (CONCAT(first_name,' ',last_name)),
+--     yearly_salary INT CHECK (yearly_salary > 0),
+--     current_status ENUM('self-employed', 'employed', 'unemployed') -- MySQL
+--     -- current_status employment_status
+-- );
+
+-- CREATE TABLE employers (
+--     -- id SERIAL PRIMARY KEY, -- posgresql
+--     id INT PRIMARY KEY AUTO_INCREMENT,
+--     company_name VARCHAR(300) NOT NULL,
+--     company_address VARCHAR(300) NOT NULL,
+--     yearly_revenue FLOAT CHECK (yearly_revenue > 0),
+--     is_hiring BOOLEAN DEFAULT FALSE
+-- );
+
+-- CREATE TABLE conversation (
+--     -- id SERIAL PRIMARY KEY, -- posgresql
+--     id INT PRIMARY KEY AUTO_INCREMENT,
+--     user_id INT,
+--     employer_id INT,
+--     message TEXT NOT NULL
+-- );
+
+INSERT INTO users (first_name,last_name,yearly_salary,current_status)
+VALUES ('Max', 'Schwarz', 19000, 'self-employed');
