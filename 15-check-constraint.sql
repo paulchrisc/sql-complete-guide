e38f3f7daf5097935b912d6f7d162398d98253dc
paulchrisc <128294135+paulchrisc@users.noreply.github.com>
2023-11-08 23:30:23 +0800

    constraints

--- 15-check-constraint.sql
+++ 15-check-constraint.sql
@@ -3,5 +3,9 @@
 --     yearly_salary INT CHECK (yearly_salary > 0)
 -- );
 
+UPDATE users
+SET yearly_salary = NULL
+WHERE full_name = 'Kahlil';
+
 ALTER TABLE users
 ADD CONSTRAINT yearly_salary_positive CHECK (yearly_salary > 0);
---
+++ 16-insert-violating-data.sql
@@ -0,0 +1,2 @@
+INSERT INTO users(full_name, yearly_salary, current_status)
+VALUES ('GG', -1000, 'unemployed');
