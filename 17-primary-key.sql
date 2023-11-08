72ed125598090dc9d0f69a7e1a5ea86e77ab1863
paulchrisc <128294135+paulchrisc@users.noreply.github.com>
2023-11-09 00:25:10 +0800

    adding primary key

---
+++ 17-primary-key.sql
@@ -0,0 +1,28 @@
+-- DROP TABLE users;
+-- DROP TABLE employers;
+-- DROP TABLE conversation;
+
+-- CREATE type employment_status as ENUM('self-employed', 'employed', 'unemployed'); -- Postgresql
+
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    full_name VARCHAR(300) NOT NULL,
+    yearly_salary INT CHECK (yearly_salary > 0),
+    current_status ENUM('self-employed', 'employed', 'unemployed') -- MySQL
+    -- current_status employment_status
+);
+
+CREATE TABLE employers (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    company_name VARCHAR(300) NOT NULL,
+    company_address VARCHAR(300) NOT NULL,
+    yearly_revenue FLOAT CHECK (yearly_revenue <0),
+    is_hiring BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE conversation (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    employer_id INT,
+    message TEXT NOT NULL
+);
