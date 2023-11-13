6c92a033690c9e85ac301c6e89e5681ea39aad61
paulchrisc <128294135+paulchrisc@users.noreply.github.com>
2023-11-13 11:25:42 +0800

    11/13

---
+++ sales-example/14-subqueries.sql
@@ -0,0 +1,9 @@
+-- CREATE VIEW base_result AS SELECT * FROM sales
+-- WHERE volume > 1000;
+
+
+-- SELECT customer_name, product_name 
+-- FROM base_result;
+
+-- INSERT INTO top_sales(...)
+-- SELECT
