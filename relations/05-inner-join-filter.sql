5adbfbf8de147b0a602a16472d3eab33da9af855
paulchrisc <128294135+paulchrisc@users.noreply.github.com>
2023-11-16 08:39:35 +0800

    left join

--- relations/05-inner-join-filter.sql
+++ relations/05-inner-join-filter.sql
@@ -2,7 +2,8 @@ SELECT u.id,u.first_name,u.last_name,u.email,a.street,a.house_number, c.name AS 
 FROM users AS u
 INNER JOIN addresses AS a ON u.address_id = a.id
 INNER JOIN cities as c ON a.city_id = c.id
-
+WHERE c.name = 'Berlin' or c.name = 'New York'
+ORDER BY u.id DESC
 
 
 
